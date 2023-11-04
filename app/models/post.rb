aba56ea81ea9ffbdda83480738dfb87b9ce834f0
Abhishek Tanwar <abhishekgangdhari@gmail.com>
2023-11-04 21:37:54 +0530

    hotwire model

---
+++ app/models/post.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  body       :text
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Post < ApplicationRecord
+    validates :body, presence: true
+    validates :title, presence: true
+end
